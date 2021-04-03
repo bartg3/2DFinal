fd79e41d063101b8172c5bb616933f5310145c25
Connor Wardell <gongarth@gmail.com>
2021-04-03 16:41:56 -0400

    Create PowerUp.cs

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    //Make sure the game object for the pickup has collision//
+    public float multiplier = 1.5f;
+    public float duration = 4.0f;
+    //Optional audioclip that plays when picked up//
+    public AudioClip powerClip;
+    //Optional particaleffect that plays once when picked up, must be created in unity//
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter3D(Collider2D other)
+    {
+        //rename this to whatever the player's tag is//
+        if (other.CompareTag("PlayerController"))
+        {
+            StartCoroutine(Pickup(other));
+        }
+    }
+
+    IEnumerator Pickup(Collider2D player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+        //Rename PlayerController inside GetComponent To whatever the player script is called//
+        PlayerController stats = player.GetComponent<PlayerController>();
+
+        stats.PlaySound(powerClip);
+        //Rename speed to whatever it is called inside the player script//
+        stats.speed *= multiplier;
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        stats.speed /= multiplier;
+
+        Destroy(gameObject);
+    }
+}
