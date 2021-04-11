325204b1f498de7233c10b3cd9a527e6c0ba8a76
RacquelChristie <70289935+RacquelChristie@users.noreply.github.com>
2021-04-10 22:55:14 -0400

    Animations added

---
+++ 2DFinalProject/Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject maincharacter;
+
+    
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+           
+            maincharacter.GetComponent<Animator>().Play("RunAnimation");
+            Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 1.0f, 1.0f);
+            transform.position = transform.position + horizontal * Time.deltaTime;
+           
+        }
+
+
+        if (Input.GetButtonDown("Vertical"))
+        {
+            maincharacter.GetComponent<Animator>().Play("JumpAnimation");
+        }
+
+    }
+
+}
